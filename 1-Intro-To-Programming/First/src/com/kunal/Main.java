80ed90dfb6e4d1cb0c3cccda7b499d335daa8892
AvishekRoy16 <avishek.roy.india@gmail.com>
2022-01-11 13:12:13 +0530

    I/O

---
+++ 1-Intro-To-Programming/First/src/com/kunal/Main.java
@@ -0,0 +1,12 @@
+package com.kunal;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("My name is Avishek");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
---
+++ 1-Intro-To-Programming/First/src/com/kunal/Primitive.java
@@ -0,0 +1,13 @@
+package com.kunal;
+
+public class Primitive {
+    public static void main(String[] args) {
+        int RollNo = 1;
+        char letter = 'r';
+        float marks = 98.64f;
+        double largedecimalnumbers = 1.12341841234;
+        long largeInteger = 3455324513241L;
+        boolean check = true;
+
+    }
+}
